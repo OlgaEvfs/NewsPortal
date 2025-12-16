392a948d70be85d922548198a1adfdc9aa2234d7
OlgaEvfs <evstafieva.olga@bk.ru>
2025-12-16 12:58:47 +0200

    The admi has created a news editing function

--- admin/controllerAdmin/controllerAdminNews.php
+++ admin/controllerAdmin/controllerAdminNews.php
@@ -16,5 +16,15 @@ class controllerAdminNews {
         $test = modelAdminNews::getNewsAdd();
         include_once('viewAdmin/newsAddForm.php');
     }
+    //------edit
+    public static function newsEditForm($id) {
+        $arr = modelAdminCategory::getCategoryList();
+        $detail = modelAdminNews::getNewsDetail($id);
+        include_once('viewAdmin/newsEditForm.php');
+    }
+    public static function newsEditResult($id) {
+        $test = modelAdminNews::getNewsEdit($id);
+        include_once('viewAdmin/newsEditForm.php');
+    }
 }// class
 ?>
